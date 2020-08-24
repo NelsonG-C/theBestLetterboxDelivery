45028510e11a4a4860a7c576ad60b868d93027b2
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-08-24 21:10:54 +1000

    seo ts fixes

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+function SEO({ description, lang, meta = [{}], title }: SEOProps) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -72,17 +72,11 @@ function SEO({ description, lang, meta, title }) {
   )
 }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
+interface SEOProps {
+  description?: string
+  lang?: string
+  meta?: Array<any>
+  title: string
 }
 
 export default SEO
