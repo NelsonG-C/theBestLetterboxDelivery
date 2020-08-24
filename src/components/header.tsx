6f254140d089d085a9ca16b2d104ff92960526f5
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-08-24 21:10:12 +1000

    added navbar to header file

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,13 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+//Bootstrap components
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+
+//Image
+import logo from "../images/gatsby-icon.png"
+
 interface HeaderProps {
   siteTitle: string
 }
@@ -13,11 +20,31 @@ const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
+    <Navbar expand="md">
+      <Navbar.Brand>
+        <img
+          alt="The best letterbox delivery"
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top mr-3"
+        />
+        The Best Letterbox Delivery
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="nav-button" />
+      <Navbar.Collapse id="nav-button" className="justify-content-end">
+        <Nav>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#services">Services</Nav.Link>
+          <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+          <Nav.Link href="#quote">Get A Quote</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
     <div
       style={{
-        margin: `0 auto`,
+        margin: `0 2rem`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -27,9 +54,7 @@ const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
             color: `white`,
             textDecoration: `none`,
           }}
-        >
-          {siteTitle}
-        </Link>
+        ></Link>
       </h1>
     </div>
   </header>
