140a76b67ea00f890c63c095731e6e4b2e1f95de
Nelson Gardner-Challis <nelsongc59@gmail.com>
2020-08-24 21:10:33 +1000

    set out layout basics

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,14 +7,14 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 import Button from "react-bootstrap/Button"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
-import logo from "../images/gatsby-icon.png"
+
+//Styles
+import "../styles.css"
 
 interface LayoutProps {
   children: any
@@ -34,41 +34,25 @@ const Layout: React.FunctionComponent<LayoutProps> = ({ children }: any) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <Navbar expand="md">
-        <Navbar.Brand>
-          <img
-            alt="The best letterbox delivery"
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top mr-3"
-          />
-          The Best Letterbox Delivery
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="nav-button" />
-        <Navbar.Collapse id="nav-button">
-          <Nav className="mr-auto">
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#services">Services</Nav.Link>
-            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
-            <Nav.Link href="#quote">Get A Quote</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          maxWidth: 1000,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <Button>Hello</Button>
-
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <Link to="home">Home</Link>
+          <p>
+            <a>0427991708</a>
+          </p>
+          <p font-size="11px">
+            <a>thebestletterboxdelivery@bigpond.com</a>
+          </p>
         </footer>
       </div>
     </>
